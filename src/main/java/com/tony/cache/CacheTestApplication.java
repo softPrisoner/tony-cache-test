c3e5dceb3e8f0d03e1bb635d48303fbac175b093
tony <wabslygzj@163.com>
2019-07-28 11:38:39 +0800

    base application

---
+++ src/main/java/com/tony/cache/CacheTestApplication.java
@@ -0,0 +1,16 @@
+package com.tony.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tony
+ * @describe CacheTestApplication
+ * @date 2019-07-28
+ */
+@SpringBootApplication
+public class CacheTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheTestApplication.class, args);
+    }
+}
